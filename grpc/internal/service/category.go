26789b214a9db00f44d68181dba5ddc712e03b5b
mvrdgs <mauricio.viegas@mercadolivre.com>
2022-12-08 09:28:53 -0300

    grpc

---
+++ grpc/internal/service/category.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"github.com/mvrdgs/go-experts/grpc/internal/database"
+	"github.com/mvrdgs/go-experts/grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return categoryResponse, nil
+}
