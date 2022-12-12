a4318fb1623c986ca847799f4a0ad886d0e9eadb
mvrdgs <mauricio.viegas@mercadolivre.com>
2022-12-12 13:11:59 -0300

    grpc stream

--- grpc/internal/service/category.go
+++ grpc/internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 	"log"
 
 	"github.com/mvrdgs/go-experts/grpc/internal/database"
@@ -18,6 +19,8 @@ func NewCategoryService(categoryDB database.Category) *CategoryService {
 		CategoryDB: categoryDB,
 	}
 }
+
+// evans -r repl
 
 func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
 	category, err := c.CategoryDB.Create(in.Name, in.Description)
@@ -61,7 +64,7 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	if err != nil {
 		return nil, err
 	}
-	
+
 	categoryResponse := &pb.Category{
 		Id:          category.ID,
 		Name:        category.Name,
@@ -69,4 +72,55 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
+}
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		err = stream.SendMsg(&pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		if err != nil {
+			return err
+		}
+	}
 }
