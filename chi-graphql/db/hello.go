5d179fc18b8e4b02cd5dd3c4946f0b0636b38694
mvrdgs <mauricio.viegas@mercadolivre.com>
2023-01-26 09:20:34 -0300

    graphql hello world

---
+++ chi-graphql/db/hello.go
@@ -0,0 +1,5 @@
+package db
+
+import "github.com/mvrdgs/go-experts/chi-graphql/graph/model"
+
+var Hello []*model.Hello
---
+++ chi-graphql/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/mvrdgs/go-experts/chi-graphql/db"
+	"github.com/mvrdgs/go-experts/chi-graphql/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Hello: db.Hello}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
